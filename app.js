7e5f294362d3837bdd94aadc5e5444787023453f
luscasnt <lucas.santana.souza28@escola.pr.gov.br>
2025-07-29 09:22:20 -0300

    salva

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-function criaCartao(Categoria, Pergunta, Resposta) {
-        let container = document.getElementById('container')
-        let cartao = document.createElement('article')
-        cartao.className = 'cartao'
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
-        cartao.innerHTML = `
-<div class="cartao_conteudo">
-<h3>${Categoria}</h3>
-<div class="cartao_conteudo_pergunta">
-        <p>${Pergunta}</p>
-</div>
-<div class="cartao_conteudo_resposta">
-        <p>${Resposta}</p>
-</div>
-</div>
-`
-container.appendChild(cartao)
+    cartao.innerHTML = `
+    <div class="cartao_conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao_conteudo_pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao_conteudo_resposta">
+    <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    let respostaEstaVisivel = false;
+    function viraCartao(){
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
+
+    cartao.addEventListener("click", viraCartao);
+    container.appendChild(cartao)
 }
